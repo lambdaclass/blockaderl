ad1c0a44e847705d9e156b2657eafa51ece16c14
unbalancedparentheses <federico.carrone@gmail.com>
2017-11-19 13:59:36 -0300

    Add test and basic functions

--- src/blockaderl.erl
+++ src/blockaderl.erl
@@ -11,62 +11,61 @@
          %% partition/4,
          %% delete_partitions/4,
          %% list/4,
-         %% status/4,
+         status/3,
+         containers_ip/3
          %% add_container/4
-         get/3
         ]).
 
-create(Host, Port, Name, Config) ->
-    Path = "blockade/" ++ Name,
-    post(Host, Port, Path, Config).
+create(Host, Port, BlockadeName, Config) ->
+    http_post(Host, Port, BlockadeName, Config).
 
-delete(Host, Port, Name) ->
-    Path = "blockade/" ++ Name,
-    http_delete(Host, Port, Path).
+delete(Host, Port, BlockadeName) ->
+    http_delete(Host, Port, BlockadeName).
+
+status(Host, Port, BlockadeName) ->
+    Result = http_get(Host, Port, BlockadeName),
+    case Result of
+        {ok, {{_, 200,"OK"}, _Headers, JsonString}} ->
+            JsonBinary = erlang:list_to_binary(JsonString),
+            Content = jsx:decode(JsonBinary, [return_maps]),
+            {ok, Content};
+        {ok, {{_, 404, _}, _, _}} ->
+            {error, not_found};
+        Error ->
+            Error
+    end.
+
+containers_ip(Host, Port, BlockadeName) ->
+    case status(Host, Port, BlockadeName) of
+        {ok, BlockadeContainers} ->
+            Containers = maps:get(<<"containers">>, BlockadeContainers, error),
+            Result = maps:map(fun(_K, V) ->
+                             maps:get(<<"ip_address">>, V)
+                     end, Containers),
+            {ok, Result};
+        Error ->
+            Error
+    end.
 
 % priv
-get(Host, _Port, Path) ->
-    Url = Host + Path,
+blockade_path() ->
+    "blockade/".
+
+url(Host, Port, Path) ->
+    "http://" ++ Host ++ ":" ++ erlang:integer_to_list(Port) ++ "/" ++ blockade_path() ++ Path.
+
+http_get(Host, Port, Path) ->
+    Url = url(Host, Port, Path),
     httpc:request(Url).
 
-post(Host, Port, Path, Body) ->
-    Url = "http://" ++ Host ++ ":" ++ erlang:integer_to_list(Port) ++ "/" ++ Path,
+http_post(Host, Port, Path, Body) ->
+    Url = url(Host, Port, Path),
     Headers = [],
     ContentType = "application/json",
     Json = jsx:encode(Body),
     httpc:request(post, {Url, Headers, ContentType, Json}, [], []).
 
 http_delete(Host, Port, Path) ->
-    Url = "http://" ++ Host ++ ":" ++ erlang:integer_to_list(Port) ++ "/" ++ Path,
+    Url = url(Host, Port, Path),
     Headers = [],
     httpc:request(delete, {Url, Headers}, [], []).
-
-
-%% Body = #{
-%%   containers => #{
-%%     reconnections => #{
-%%       image => <<"erlang:20">>,
-%%       volumes => #{
-%%         <<".">> => <<"/opt/reconnections">>
-%%       },
-%%       command => <<"sleep infinity">>
-%%     },
-%%     postgres => #{
-%%       image => <<"postgres">>,
-%%       ports => #{
-%%         <<"5432">> => 5432
-%%       },
-%%       environment => #{
-%%         <<"POSTGRES_PASSWORD">> => <<"example">>
-%%       }
-%%     },
-%%     redis => #{
-%%       image => <<"redis:alpine">>,
-%%       <<"ports">> => #{
-%%         <<"6379">> => 6379
-%%       }
-%%     }
-%%   }
-%%  },
-%% blockaderl:create("localhost", 5000,  "a", Body).
-%% blockaderl:delete("localhost", 5000, "a").
---
+++ test/basic_SUITE.erl
@@ -0,0 +1,38 @@
+-module(basic_SUITE).
+
+-export([all/0]).
+-export([basic/1]).
+
+all() ->
+    [Fun || {Fun, 1} <- module_info(exports), Fun =/= module_info].
+
+basic(_Config) ->
+    BlockadeContainers = #{
+      containers => #{
+        reconnections => #{
+          image => <<"erlang:20">>,
+          volumes => #{
+            <<".">> => <<"/opt/reconnections">>
+           },
+          command => <<"sleep infinity">>
+         },
+        postgres => #{
+          image => <<"postgres">>,
+          ports => #{
+            <<"5432">> => 5432
+           },
+          environment => #{
+            <<"POSTGRES_PASSWORD">> => <<"example">>
+           }
+         },
+        redis => #{
+          image => <<"redis:alpine">>,
+          <<"ports">> => #{
+              <<"6379">> => 6379
+             }
+         }
+       }
+     },
+    blockaderl:create("localhost", 5000, "a", BlockadeContainers),
+    blockaderl:delete("localhost", 5000, "a"),
+    ok.
